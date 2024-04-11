e4276d66df7b8a718fadb17adf64606a79553dce
Priya <priyapiyuse@gmail.com>
2024-04-10 17:09:38 -0700

    useContext used

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useContext } from "react";
+import { CartContext } from "./providers/CartProviders";
 
 const CartScreen = () => {
+  const { items } = useContext(CartContext);
   return (
     <View>
-      <Text style={{ color: "white" }}>cart</Text>
+      <Text style={{ color: "white" }}>cart items length:{items.lenght}</Text>
+
+      <StatusBar style={Platform.OS == "ios" ? "light" : "auto"} />
     </View>
   );
 };
